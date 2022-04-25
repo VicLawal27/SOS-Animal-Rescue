cfd78816602eac16ed042450c1dda7cb7a27657a
Olivia Lammam <olivia.l7475@gmail.com>
2022-04-25 00:04:08 -0700

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,40 @@ async function loadDogPic()  {
 
 }
 
+const rememberUser = document.querySelector('.remember');
+const form = document.querySelector('form');
+const getName = document.querySelector('#entername');
+const getEmail = document.querySelector('#enteremail');
+const getPhone = document.querySelector('#enterphone');
+const submitBtn = document.querySelector('#submitinput')
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
+
+submitBtn.addEventListener('click', function () {
+localStorage.setItem('name', getName.value);
+localStorage.setItem('email', getEmail.value);
+localStorage.setItem('phone', getPhone.value);
+
+nameDisplayCheck();
+})
+
+function nameDisplayCheck() {
+    if (localStorage.getItem('name')) {
+        let name = localStorage.getItem('name');
+    }
+}
+
+// var input = document.getElementsByClassName('formInput').value;
+// localStorage.setItem('name', input);
+// localStorage.setItem('email', input);
+// localStorage.setItem('phone', input);
+
+// document.getElementsByClassName('formInput').value = localStorage.getItem('name');
+// document.getElementsByClassName('formInput').value = localStorage.getItem('email');
+// document.getElementsByClassName('formInput').value = localStorage.getItem('phone');
+
 /*const options = {
     headers: {
         'Content-Type': 'text/plain',
